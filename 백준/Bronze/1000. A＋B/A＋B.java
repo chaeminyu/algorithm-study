be4cce6189e618378d6cb400581076331b49a3f5
Chaemin Yu <113892409+chaeminyu@users.noreply.github.com>
2024-01-31 01:35:58 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main{
+     public static void main(String[] args){
+         Scanner num = new Scanner(System.in);
+         int A = num.nextInt();
+         int B = num.nextInt();
+         System.out.println(A+B);
+     }
+ }
